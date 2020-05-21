957055e284116bb451a5c67984133dad9a912fb0
Ingo Kuba <ingo.kuba@yahoo.de>
2020-05-21 06:36:02 +0200

    Add first ttl triples for sherlock. (#1)
    * Add first ttl triples for sherlock.
    
    * Add ci to check ttl syntax.

---
+++ turtle-validator.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+output=$(npm run ttl)
+if [[ $output == *"0 warnings and 0 errors"* ]]; then
+  echo "Turtle syntax correct."
+  exit 0
+else
+  echo "Turtle validator failed!"
+  exit 1
+fi
